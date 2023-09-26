260eba183cfca280891365fabdfa1942d0e7a528
SamueleGonella <144803411+SamueleGonella@users.noreply.github.com>
2023-09-26 17:08:31 +0200

    Prima Foto Samu

--- src/app/modules/echarts/vertical-bars-timeline/big-tree.component.ts
+++ src/app/modules/echarts/vertical-bars-timeline/big-tree.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from "@angular/core";
 import * as echarts from 'echarts';
 import { Observable } from "rxjs";
 import { IInformations } from "./informations";
+import { EChartsOption } from "echarts";
 
 @Component({
     selector: 'big-tree-comp',
@@ -10,36 +11,82 @@ import { IInformations } from "./informations";
   })
 
 export class BigTreeChart {
-  InfosURL = 'https://mocki.io/v1/9da83f53-bce5-4f04-a09d-855cd8577d44';
-  //InfosURL = 'https://mocki.io/v1/07a0d582-3a16-4838-adef-5cec3d22f574'; //subFILE.json online
-  ObservInfos!: Observable<IInformations[]>;
-  Infos!:IInformations[];
+  echartsInstance: any;
 
-  constructor(private _http: HttpClient) {
+  onChartInit(ec: any) {
+    this.echartsInstance = ec;
+  }
+  
+  resizeChart() {
+    if (this.echartsInstance) {
+      this.echartsInstance.resize();
+    }
   }
 
-  getInfos(): Observable<IInformations[]> {
-      console.log('Doing the getInfos');
-      this.ObservInfos = this._http.get<IInformations[]>(this.InfosURL); 
-      console.log('    ');
-      console.log('    ');
-      console.log('Observ: ');
-      console.log(this.Infos);
-      console.log('    ');
-      return this.ObservInfos;
-  }
+chartOption1: EChartsOption = {
+  color: ['#ffffff', '#804000', '#cc6600', '#ff8c1a', '#ffb366' ],
+    title: {
+        text: 'ENERGIA ATTIVA'
+    },
+    tooltip: {    //per sottolineare linee verticali per settori
+      trigger: 'axis',  //givva piccola label quando su un asse
+      axisPointer: {
+        type: 'shadow', //tipo di sottolineamento
+        label: {
+          backgroundColor: '#6a7985', //NON LO SO
+          distance: 2,
+          formatter: '{a}: {@score}',
+        }
+      }
+    },
+  grid: {
+    
+  },
+  xAxis: {
+    type: 'category',
+    boundaryGap: true,
+    data: ['SET', 'OTT', 'NOV', 'DIC', 'GEN', 'FEB', 'MAR', 'APR', 'MAG', 'GIU', 'LUG', 'AGO'],
+  },
+  yAxis: {
+    type: 'value',
+    min:0,
+    max:60000,
 
-  ngOnInit(): void {
-      console.log('In OnInit');
-      this.getInfos()
-              .subscribe( info => {
-                  this.Infos = info;
-                }
-              );           
-      console.log('    ');
-      console.log('Products: ');
-      console.log(this.Infos);
-      console.log('    ');
-      console.log('    ');
-  }
+  },
+  
+  series: [
+    {
+      name: 'Attività Totale',
+      type: 'bar',
+      stack: 'Total',  //mette dati uno sopra l'altro per somma di dati (es, vendite di tipi di prodotti sommate)
+      
+    },
+    {
+      name: 'Attiva F3',
+      type: 'bar',
+      stack: 'Total',  //mette dati uno sopra l'altro per somma di dati (es, vendite di tipi di prodotti sommate)
+      data: [9000, 10000, 8000, 11000, 10000, 7000, 14000, 13000, 11000, 10000, 10000, 14000]
+    },
+    {
+      name: 'Attiva F2',
+      type: 'bar',
+      stack: 'Total',  //mette dati uno sopra l'altro per somma di dati (es, vendite di tipi di prodotti sommate)
+      data: [6000, 10000, 10000, 12000, 10000, 15000, 14000, 13000, 11000, 10000, 9000, 14000]
+    },
+    {
+      name: 'Attiva F1',
+      type: 'bar',
+      stack: 'Total',  //mette dati uno sopra l'altro per somma di dati (es, vendite di tipi di prodotti sommate)
+      data: [8000, 10000, 11000, 9000, 10000, 8000, 14000, 13000, 11000, 10000, 13000, 14000]
+    },
+    {
+      name: 'Attiva Mono',
+      type: 'bar',
+      stack: 'Total',  //mette dati uno sopra l'altro per somma di dati (es, vendite di tipi di prodotti sommate)
+      data: [10000, 10000, 7000, 10000, 12000, 15000, 14000, 13000, 11000, 10000, 10000, 14000]
+    },
+    
+  ],
+  
+}
 }
